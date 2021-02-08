9d849008a4db4a2a8a5a2752b9a180fcb8a98f29
Daniel Tavares de Lima Marcelino <toys.tokio@gmail.com>
2021-02-08 13:25:49 -0300

    Corrigir o código

--- Temperatura/index.py
+++ Temperatura/index.py
@@ -1,33 +1,26 @@
-c = str(input("Qual medida você deseja converter?"))
-h = str(input("Celsius para Fahrenheit, digite: C"))
-ha = str(input("Fahrenheit para Celsius, digite: F"))
-haa = str(input("Celsius para Kelvin, digite: Ck"))
-ho = str(input("Kelvin para Celsius, digite: Kc")) 
-hoo = str(input("Fahrenheit para Kelvin, digite Fk"))
-hi = str(input("Kelvin para Fahrenheit, digite:Kf))
-hii = str(input("Digite aqui:"))
-if h == "C":
+c = str(input("Qual medida você deseja converter?\n Celsius para Fahrenheit, digite: C\n Fahrenheit para Celsius, digite: F\n Celsius para Kelvin, digite: Ck\n Kelvin para Celsius, digite: Kc\n Fahrenheit para Kelvin, digite: Fk\n Kelvin para Fahrenheit, digite: Kf\n Digite aqui:"))
+if c == "C":
 	ce = float(input("Digite a temperatura em Celsius:"))
 	t = ((ce * 9) / 5) + 32
 	print(t, "graus Fahrenheit")
-if ha == "F":
+if c == "F":
 	fa = float(input("Digite a temperatura em Fahrenheit:"))
 	g = (fa - 32) / 1.8
 	print(g, "graus Celsius")
-if haa == "Ck":
+if c == "Ck":
 	ke = float(input("Digite a temperatura em Celsius:"))
 	p = ke + 273
 	print(p, "graus kelvin")
-if ho == "Kc":
+if c == "Kc":
 	Kel = float(input("Digite a temperatura em Kelvin:"))
 	q = Kel - 273
 	print(q, "graus Celsius")
-if hoo == "Fk":
+if c == "Fk":
 	fel = float(input("Digite a temperatura em Fahrenheit:"))
 	x = (fel - 32) / 1.8
 	xz = x + 273
 	print(xz, "graus Kelvin")
-if hi == "Kf":
+if c == "Kf":
 	kelva = float(input("Digite a temperatura em Kelvin:"))
 	z = ((kelva * 9) / 5) + 32
 	zx = z + 273
